66db455b5ac3e0c8c5139904b7c89c79ae6e1a7d
evanvvav <ivankolesov.fi@gmail.com>
2021-09-15 23:47:01 +0300

    1 commit

---
+++ src/main/java/com/example/bookStore/web/bookStoreController.java
@@ -0,0 +1,16 @@
+package com.example.bookStore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+
+public class bookStoreController {
+
+	@RequestMapping(value = "/bookStoreIndex", method = RequestMethod.GET)
+	public String hello() {
+		return "bookStoreIndex";
+	}
+
+}
